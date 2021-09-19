6f9cd305d37e23f0a7f6b7668ec3fdb19d75897a
Thomas21119 <thomas.wittwer2018@gmail.com>
2021-09-19 15:38:04 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var $currentDate = moment().format("ddd MMM Do, YYYY");
+$("#currentDay").text($currentDate);
+var time = $('#time').text()
+
+
+console.log(Number(moment().format("HH")))
+
+time = $('.time')
+
+// function test() {
+// setInterval(function() {
+//     if (Number(moment().format("HH"))<10) {
+//     document.querySelector('body').setAttribute('style', 'background-color: red;')
+//     console.log(Number($time.text()))
+//     } else {
+//         console.log('not working')
+//     }
+
+//     },1000);
+// }
+
+test()
+
+function colorCoding() {
+    let i =  $("#taskInput");
+for (i = 0 ; i < time.length ; i++){
+    var d = new Date();
+    var n = d.getUTCHours() + 1;
+    if (n < time.text()) {
+        // If time.text() is after 6PM or before 9AM, apply night theme to 'body'
+        $("#taskInput").addClass("past").removeClass("present", 'future');
+    } else if (n = time.text()) {
+        // Else use ‘day’ theme
+        $("#taskInput").addClass("present").removeClass("past" , "future");
+    } else if (n > time.text()){
+        $("#taskInput").addClass("future").removeClass("past" , "present");
+    }
+}
+}
+$(document).ready(function () {
+    colorCoding();
+});
+
+console.log("time length" + time.length)
