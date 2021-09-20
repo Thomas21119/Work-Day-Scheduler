9530b2479ed99e46051cc08d96b27beae10263e3
Thomas21119 <thomas.wittwer2018@gmail.com>
2021-09-20 18:06:53 +1000

    core functionality down

--- Assets/README.md
+++ Assets/README.md
@@ -23,13 +23,13 @@ WHEN I open the planner
 WHEN I scroll down
 <!-- THEN I am presented with timeblocks for standard business hours -->
 WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+<!-- THEN each timeblock is color coded to indicate whether it is in the past, present, or future -->
 WHEN I click into a timeblock
-THEN I can enter an event
+<!-- THEN I can enter an event -->
 WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
+<!-- THEN the text for that event is saved in local storage -->
 WHEN I refresh the page
-THEN the saved events persist
+<!-- THEN the saved events persist -->
 ```
 
 The following animation demonstrates the application functionality:
--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 var $currentDate = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text($currentDate);
 
-
 const momentTime = moment().hours()
-console.log(momentTime)
-time = $('.time')
-
-console.log(time[0])
-// function test() {
-// setInterval(function() {
-//     if (Number(moment().format("HH"))<10) {
-//     document.querySelector('body').setAttribute('style', 'background-color: red;')
-//     console.log(Number($time.text()))
-//     } else {
-//         console.log('not working')
-//     }
-
-//     },1000);
-// }
+// const momentTime = 15
 
 function colorCoding(){
     var taskInput =  $(".taskInput");
-    for (i = 0 ; i < time.length ; i++){
-        if (momentTime < time[i]) {
-            console.log(momentTime)
-            console.log(time[i])
+        taskInput.each(function(){
+            var time = $(this).data('hour')
+        if (momentTime > time) {
         $(this).addClass("past").removeClass("present", 'future');
-    } else if (momentTime == time[i]) {
-        console.log(momentTime)
-        console.log(time[i])
+    } else if (momentTime == time) {
         $(this).addClass("present").removeClass("past" , "future");
     } else {
         $(this).addClass("future").removeClass("past" , "present");
-        console.log(momentTime)
-        console.log(time[i])
+    
     }
+})
 }
+
+var btnClick = $('.saveBtn')
+btnClick.on("click", save);
+
+function save() {
+    var input = $('.taskInput')
+    input.each(function() {
+        input9 = $(this).data('hour')
+        localStorage.setItem('input hour' + input9, this.value)
+    })
+}
+
+function loadPage() {
+    var input = $('.taskInput')
+    input.each(function() {
+        input9 = $(this).data('hour')
+        this.value = localStorage.getItem('input hour' + input9)
+    })
 }
 
 $(document).ready(function () {
     colorCoding();
+    loadPage();
 });
+
